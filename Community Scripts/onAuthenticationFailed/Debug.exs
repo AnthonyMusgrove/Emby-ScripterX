73dc46f552bb70ba37b7f3fb4a3b4d9535a729f2
Anthony Musgrove <55648663+AnthonyMusgrove@users.noreply.github.com>
2020-04-13 01:15:12 +1000

    Create Debug.exs

---
+++ Community Scripts/onAuthenticationFailed/Debug.exs
@@ -0,0 +1,10 @@
+{
+	"id": 1
+	"name":	"Script Debug",
+	"event": "onAuthenticationFailed",
+	"description": "This is just a debug script for the community script library"
+	"author": {
+		"name": "Anthony Musgrove",
+		"email": "anthony@akjm.photography",
+	}
+}
