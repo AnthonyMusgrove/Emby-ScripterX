3bb59a8f70abdc3556a02a14dadefea184229299
Anthony Musgrove <55648663+AnthonyMusgrove@users.noreply.github.com>
2020-04-19 23:49:13 +1000

    Add files via upload

---
+++ ExampleScripts/Bash/media.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Parameters expected: <action:add/remove/update> %item_id% %item.name% %item.path%
+
+echo Media $1 : [ID: $2] [Name: $3] [Path: $4] >> /home/medius/scripts/media.log
+
---
+++ ExampleScripts/Bash/playback.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Parameters expected: Parameters expected:  <action:start/stop> %username% %device.name% %item.id% %item.name%
+
+echo Playback $1 : User: $2 on device $3 - media: [ID: $4] $5 >> /home/medius/scripts/playback.log
+
