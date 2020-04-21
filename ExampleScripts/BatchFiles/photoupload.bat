a9fda39dfe2f83f27d626f52eca36fa1447fd6fb
Anthony Musgrove <55648663+AnthonyMusgrove@users.noreply.github.com>
2020-04-21 22:46:53 +1000

    photoupload.bat - for an example onCameraImageUploaded

---
+++ ExampleScripts/BatchFiles/photoupload.bat
@@ -0,0 +1,2 @@
+REM Parameters expected:  %device.last.username% %photo.name% %photo.mime.type %photo.album% %device.name% 
+@echo %1 uploaded %2 (type: %3) to %4 from device %5 >> D:\embyscripts\photoupload.log
