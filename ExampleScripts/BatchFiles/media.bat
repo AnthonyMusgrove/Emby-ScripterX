0880e92e581fbd2cb43e18f9e3bfaa82b8659888
Anthony Musgrove <55648663+AnthonyMusgrove@users.noreply.github.com>
2020-04-23 12:45:23 +1000

    Update media.bat

--- ExampleScripts/BatchFiles/media.bat
+++ ExampleScripts/BatchFiles/media.bat
@@ -1,2 +1,2 @@
-REM Parameters expected:  <action:add/remove/update> %item_id% %item.name% %item.path%
+REM Parameters expected:  <action:add/remove/update> %item.id% %item.name% %item.path%
 @echo Media %1 : [ID: %2] [Name: %3] [Path: %4] >> D:\embyscripts\media.log
